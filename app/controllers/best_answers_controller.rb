9d83d79612553a6524429fa3bc7cdec76b4471d2
klcode1001 <ledev0726@gmail.com>
2016-06-23 05:00:14 +0900

    Add test

--- app/controllers/best_answers_controller.rb
+++ app/controllers/best_answers_controller.rb
@@ -2,7 +2,8 @@ class BestAnswersController < ApplicationController
   def best
     note = Note.find(params[:note_id])
     bestanswer = note.build_best_answer(comment: params[:comment])
+    # bestanswer = note.build_best_answer(comment_id: params[:comment])
     bestanswer.save
-    redirect_to (:back), notice: 'hogehoge'
+    redirect_to note, notice: 'hogehoge'
   end
 end
---
+++ test/integration/best_answer_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class BestAnswerTest < ActionDispatch::IntegrationTest
+  setup do
+    @note = note = Note.create! body: 'Hello?'
+    @comment_1 = note.comments.create! body: 'Hello!'
+    @comment_2 = note.comments.create! body: 'Hello!!'
+    @comment_3 = note.comments.create! body: 'Hello!!!'
+  end
+
+  test "best answer" do
+    get root_path
+    assert_template 'notes/index'
+
+    get note_path(@note)
+    assert_template 'notes/show'
+
+    post best_path comment: @comment_1.id, note_id: @note.id
+    assert_redirected_to @note
+
+    assert_equal @comment_1, @note.reload.best_answer.comment
+  end
+end
