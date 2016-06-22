b3c91e7ef163b6a884107ac0ca761cc376f4f09e
klcode1001 <ledev0726@gmail.com>
2016-06-23 07:19:39 +0900

    Remove comment

--- app/controllers/best_answers_controller.rb
+++ app/controllers/best_answers_controller.rb
@@ -2,7 +2,6 @@ class BestAnswersController < ApplicationController
   def best
     note = Note.find(params[:note_id])
     bestanswer = note.build_best_answer(comment: params[:comment])
-    # bestanswer = note.build_best_answer(comment_id: params[:comment])
     bestanswer.save
     redirect_to note, notice: 'hogehoge'
   end
